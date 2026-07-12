5a0f65d112a250590348ea5a8e49678e8245c456
Idkthisguy <idkf0ul@gmail.com>
2026-07-12 19:32:26 -0400

    Components part 1
    Components now can be added to nodes like in unity, part 2 will be actually making them functional

--- Flux/editor/properties.h
+++ Flux/editor/properties.h
@@ -11,35 +11,8 @@
 #include <glm/gtx/quaternion.hpp>
 
 namespace Flux {
-	struct TransformComponent {
-		glm::vec3 position;
-		glm::vec3 rotation;
-		glm::vec3 scale;
-	};
-
-	struct CameraComponent {
-		float fov;
-		bool  isMainCamera;
-	};
-
-	struct MeshComponent {
-		std::string modelPath;
-		float roughness;
-		float metallic;
-	};
-
-	struct PhysicsComponent {
-		glm::vec3 velocity;
-		bool isAnchored;
-	};
-
-	struct Component {
-		std::string name;
-		std::variant<TransformComponent, CameraComponent, MeshComponent, PhysicsComponent> data;
-	};
-
 	class Heiarchy;
-
+	
 	class Properties {
 	public:
 		void renderProperties(Heiarchy* h = nullptr);
